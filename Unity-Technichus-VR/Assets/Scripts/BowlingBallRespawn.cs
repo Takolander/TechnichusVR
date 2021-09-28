d9dfc7418b3e60d2e164815fd73b1469b42613f0
Seranz <kadamani88@hotmail.com>
2021-09-28 11:00:38 +0200

    Respawn on Worldfloor
    Fixed respawn on worldfloor
    Testing speedlimit script

--- Unity-Technichus-VR/Assets/Scripts/BowlingBallRespawn.cs
+++ Unity-Technichus-VR/Assets/Scripts/BowlingBallRespawn.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 using UnityEngine.XR;
 public class BowlingBallRespawn : MonoBehaviour
 { 
+    float x = -0.02000046f;
+    float y = 0.125f;
+    float z = 1.609996f;
+    float minimumSpeed = 0.5f;
+    
     void OnCollisionEnter(Collision other) {
-        if(other.gameObject.name == "backWall"){
+        if(other.gameObject.name == "backWall" || other.gameObject.name == "Plane"){
             Invoke("SpawnNext", 2f); 
-        } 
+        }
     }
+       /*if (interactable != null && interactable.attachedToHand != null){
+            if (interactable.attachedToHand = null){*/
+    void FixedUpdate(){
+            Vector3 startPos = new Vector3(x, y, z);
+            var speed = Vector3.Distance(startPos, transform.position) * 100f;
+            startPos = transform.position;
+            Debug.Log("Speed: " + speed.ToString("F2"));
+
+            if(speed < minimumSpeed){
+                Invoke("SpawnNext", 2f);
+            }
+    }
+    
     void SpawnNext()
     {
         GameObject newBox = Instantiate(gameObject);
-        newBox.transform.position = new Vector3(-0.02000046f, 0.125f, 1.609996f);
+        newBox.transform.position = new Vector3(x, y, z);
         Destroy(gameObject);
     }
 }
