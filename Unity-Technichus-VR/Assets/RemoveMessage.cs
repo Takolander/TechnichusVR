adca1840ee062669c6540f26618e7b77215537ac
Takolander <william_tako@hotmail.com>
2021-09-30 16:54:33 +0200

    Fix message in headset
    Fixed so that the message in headset fully working in disappearing after tilting head to look at controllers, need adjustment
    
    Co-Authored-By: marksman97 <58526577+marksman97@users.noreply.github.com>

--- Unity-Technichus-VR/Assets/RemoveMessage.cs
+++ Unity-Technichus-VR/Assets/RemoveMessage.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour
+public class RemoveMessage : MonoBehaviour
 {
     public GameObject gameObject;
+    public GameObject camera;
     public float Timer = 5.0f;
     // Update is called once per frame
     public IEnumerator Wait() {
         //Wait for 5 seconds
+        
         yield return new WaitForSeconds(Timer);
         //gameObject.SetActive(false);
         Destroy(gameObject);
     }
     void Update()
     {
-        if(transform.rotation.x > -1){
+        //adjust the value more for middle
+        if(camera.transform.rotation.x > 0.3){
             StartCoroutine(Wait());
+            
         }
     }
 }
