2b4a68d0636c255978ba9bbe03382bed5e9a95c9
marksman97 <robindahlgren007@gmail.com>
2021-10-13 16:53:39 +0200

    Using demo room now
    Use the demo room now and gave the tv my function of warning change delay and door handle change

--- Unity-Technichus-VR/Assets/DoorHandle.cs
+++ Unity-Technichus-VR/Assets/DoorHandle.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;  
 
 public class DoorHandle : MonoBehaviour {  
-    public string sceneName;
+    public string sceneName = "Scenes/Bowling";
     void OnTriggerEnter(Collider other) {
         //Checks if the bowlingball has colided with the backwall or the floor and calls respawn function
         if(other.gameObject.name == "door_handle") {
-            LoadScene(sceneName);
+            SceneManager.LoadScene(sceneName);
+            Debug.Log("Inne i if satsen");
+            //LoadScene(sceneName);
         }
+        Debug.Log("Inne i ontrigger");
     }
     //Load an Scene
     public void LoadScene(string sceneName) {  
         //SceneManager.LoadScene(sceneName);  
         SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
-    }  
+    }
 }  
